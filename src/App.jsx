2b599ccc2d7f8c587d406aaa889ac355bdf19b45
Aleikson <aleiksonsilva@hotmail.com>
2024-03-25 14:04:57 -0300

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Search from './pages/Search';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
