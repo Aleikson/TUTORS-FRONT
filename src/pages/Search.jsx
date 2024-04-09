abc3256ccd0f47aee9190413c68cc1fe3fd32903
Aleikson <aleiksonsilva@hotmail.com>
2024-04-08 22:22:01 -0300

    Feat sidebar search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,112 @@
+import { Button, Select, TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 export default function Search() {
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: '',
+    sort: 'desc',
+    category: 'Sem categoria',
+  });
+
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    const sortFromUrl = urlParams.get('sort');
+    const categoryFromUrl = urlParams.get('category');
+    if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+      setSidebarData({
+        ...sidebarData,
+        searchTerm: searchTermFromUrl,
+        sort: sortFromUrl,
+        category: categoryFromUrl,
+      });
+    }
+
+    const fetchPosts = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/post/getposts?${searchQuery}`);
+      if (!res.ok) {
+        setLoading(false);
+        return;
+      }
+      if (res.ok) {
+        const data = await res.json();
+        setLoading(false);
+        if (data.posts.length === 9) {
+          console.log('test');
+        }
+      }
+    };
+    fetchPosts();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    if (e.target.id === 'searchTerm') {
+      setSidebarData({ ...sidebarData, searchTerm: e.target.value });
+    }
+    if (e.target.id === 'sort') {
+      const order = e.target.value || 'desc';
+      setSidebarData({ ...sidebarData, sort: order });
+    }
+    if (e.target.id === 'category') {
+      const category = e.target.value || 'uncategorized';
+      setSidebarData({ ...sidebarData, category });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', sidebarData.searchTerm);
+    urlParams.set('sort', sidebarData.sort);
+    urlParams.set('category', sidebarData.category);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
-    <div>Search</div>
-  )
+    <div className='flex flex-col md:flex-row'>
+      <div className='p-7 border-b md:border-r md:min-h-screen'>
+        <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+          <div className='flex   items-center gap-2'>
+            <TextInput
+              placeholder='Pesquisar...'
+              id='searchTerm'
+              type='text'
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='flex items-center gap-2'>
+            <label className='font-semibold'>Filtrar:</label>
+            <Select onChange={handleChange} value={sidebarData.sort} id='sort'>
+              <option value='desc'>Novos artigos</option>
+              <option value='asc'>Velhos artigos</option>
+            </Select>
+          </div>
+          <div className='flex items-center gap-2'>
+            <label className='font-semibold'>Category:</label>
+            <Select
+              onChange={handleChange}
+              value={sidebarData.category}
+              id='category'
+            >
+              <option value='uncategorized'>Sem Categoria</option>
+              <option value='reactjs'>React.js</option>
+              <option value='javascript'>JavaScript</option>
+            </Select>
+          </div>
+          <Button type='submit' outline className='bg-primary'>
+            Filtrar
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
