20d25cda25c4cabef97a3b1851b159fa35d70957
Aleikson <aleiksonsilva@hotmail.com>
2024-04-10 22:02:51 -0300

    Feat admin route & create post admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import { SignIn } from "./pages/SignIn";
-import { SignUp } from "./pages/SignUp";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Search from "./pages/Search";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Search from './pages/Search';
+import CreatePost from './pages/CreatePost';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/registrar" element={<SignUp />} />
-        <Route path="/pesquisar" element={<Search />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/registrar' element={<SignUp />} />
+        <Route path='/pesquisar' element={<Search />} />
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/criar-post-admin' element={<CreatePost />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
