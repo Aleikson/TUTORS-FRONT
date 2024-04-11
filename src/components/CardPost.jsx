1cba41f1ba15ce6dc33f490eb2ab545ac9a71019
Aleikson <aleiksonsilva@hotmail.com>
2024-04-10 22:02:05 -0300

    Feat card posts

--- src/components/CardPost.jsx
+++ src/components/CardPost.jsx
@@ -1,43 +1,27 @@
-import { Card } from "flowbite-react";
-import reacticon from "../assets/reacticon.png";
-import { Link } from "react-router-dom";
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
 
-export default function CardPost() {
+export default function CardPost({ post }) {
   return (
-    <Card
-      className="group relative w-full border border-teal-500 hover:border-2 h-[430px] overflow-hidden rounded-lg sm:w-[430px] 
-    p-0"
-    >
-      <Link>
+    <div className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all'>
+      <Link to={`/post/${post.slug}`}>
         <img
-          className="h-[260px] w-full  object-cover z-20"
-          src={reacticon}
-          alt=""
+          src={post.image}
+          alt='post cover'
+          className='h-[260px] w-full  object-cover group-hover:h-[200px] transition-all duration-300 z-20'
         />
       </Link>
-      <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {/* {index.title} */}
-        Titulo1
-      </h2>
-      <div className="p-3 flex flex-col gap-2">
-        <p className="text-lg font-semibold line-clamp-2 ">
-          {/* {index.description} */}
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological orasasaderHere are the biggest
-          enterprise technology acquisitions of 2021 so far, in reverse
-          chronological orasasaderHere are the biggest enterprise technology
-          acquisitions of 2021 so far, in reverse chronological orasasaderHere
-          are the biggest enterprise technology acquisitions of 2021 so far, in
-          reverse chronological orasasaderHere are the biggest enterprise
-          technology acquisitions of 2021 so far, in reverse chronological
-          orasasaderHere are the biggest enterprise technology acquisitions of
-          2021 so far, in reverse chronological orasasaderHere are the biggest
-          enterprise technology acquisitions of 2021 so far, in reverse
-          chronological orasasaderHere are the biggest enterprise technology
-          acquisitions of 2021 so far, in revers
-        </p>
-        <span className="italic text-sm">javascript</span>
+      <div className='p-3 flex flex-col gap-2'>
+        <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
+        <p className='text-sm line-clamp-2'>{post.content}</p>
+        <span className='italic text-sm'>{post.category}</span>
+        <Link
+          to={`/post/${post.slug}`}
+          className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
+        >
+          Read article
+        </Link>
       </div>
-    </Card>
+    </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,53 @@
-import { Button } from "flowbite-react";
-import CardPost from "../components/CardPost";
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import CardPost from '../components/CardPost';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <section>
-      <div className="flex-col sm:m-auto md:m-auto 2xl:m-auto  w-3/5 ">
-        <div className="ml-12">
-          <h1 className="text-4xl mt-10 ">Lorem Impsum Lorem impsum</h1>
-          <p className="mt-3">lorem impsum lorem impsum lorem impsum</p>
-        </div>
-
-        <div className="w-4/5 mt-10 ml-12 ">
-          <Button className="w-28 bg-primary" size="xs">
-            Ver mais
-          </Button>
-        </div>
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Lorem Impsum</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s.
+        </p>
+        <Link
+          to='/search'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+        >
+          Ver todos os artigos
+        </Link>
       </div>
-
-      <div className="max-w-6xl  mx-auto p-3 flex  flex-col gap-8 py-7">
-        <div className="flex justify-center gap-4">
-          <CardPost />
-        </div>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+            <div className='flex flex-wrap gap-4'>
+              {posts.map((post) => (
+                <CardPost key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              Ver todos os artigos
+            </Link>
+          </div>
+        )}
       </div>
-      <a className="flex justify-center mt-10 text-primary cursor-pointer">
-        Veja todos os artigos
-      </a>
-    </section>
+    </div>
   );
 }
