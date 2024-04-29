289bad94d09de091f500b3822e9e32173c00cfe2
Aleikson <aleiksonsilva@hotmail.com>
2024-04-29 20:42:48 -0300

    Feat about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <h1>Sobre</h1>
+      </div>
+    </div>
+  );
+}
