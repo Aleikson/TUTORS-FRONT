915db8b7e6697ef5a92ce03fe495abc49d6eed43
Aleikson <aleiksonsilva@hotmail.com>
2024-04-29 20:42:59 -0300

    Feat projects page

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Em Breve</h1>
+    </div>
+  );
+}
